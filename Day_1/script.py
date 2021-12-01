234e83cb3f1657aca8abe6b78727d407d5db88ca
GiM6114 <73024037+GiM6114@users.noreply.github.com>
2021-12-01 19:30:12 +0100

    Day1

---
+++ Day_1/script.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+with open('input.txt', 'r') as f:
+    depths = f.read()
+
+depths = np.fromstring(depths, dtype='uint16', sep='\n')
+
+# =============================================================================
+# PART 1
+# =============================================================================
+
+count = 0  
+for idx, depth in enumerate(depths[1:]):
+    if depth > depths[idx]:
+        count += 1
+        
+print(count)
+
+# =============================================================================
+# PART 2
+# =============================================================================
+
+count = 0
+previousSum = sum(depths[0:3])
+for idx,depth in enumerate(depths[1:-1]):
+    currentSum = sum(depths[idx:idx+3])
+    if currentSum > previousSum:
+        count += 1
+    previousSum = currentSum
+
+print(count)
