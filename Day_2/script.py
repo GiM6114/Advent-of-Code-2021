98ad7a29321fc35dcab1cd56d7c090babf4e07f6
GiM6114 <73024037+GiM6114@users.noreply.github.com>
2021-12-04 13:31:27 +0100

    Erased useless parameter in loop

--- Day_2/script.py
+++ Day_2/script.py
@@ -4,29 +4,16 @@ import numpy as np
 with open('input.txt', 'r') as f:
     depths = f.read()
 
-depths = np.fromstring(depths, dtype='uint16', sep='\n')
+
 
 # =============================================================================
 # PART 1
 # =============================================================================
 
-count = 0  
-for idx, depth in enumerate(depths[1:]):
-    if depth > depths[idx]:
-        count += 1
+
         
 print(count)
 
 # =============================================================================
 # PART 2
 # =============================================================================
-
-count = 0
-previousSum = sum(depths[0:3])
-for i in range(len(depths[1:-1])):
-    currentSum = sum(depths[i:i+3])
-    if currentSum > previousSum:
-        count += 1
-    previousSum = currentSum
-
-print(count)
