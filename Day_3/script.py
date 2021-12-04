c530c697a8c4cf8300fb392f124cc9ca745d2bed
GiM6114 <73024037+GiM6114@users.noreply.github.com>
2021-12-04 19:46:10 +0100

    day3 fix

--- Day_3/script.py
+++ Day_3/script.py
@@ -1,34 +1,75 @@
-import numpy as np
-
 with open('input.txt', 'r') as f:
-    reports = np.genfromtxt(f,dtype='uint8',delimiter=1)
+    reports = f.read()
+
+reports = reports.split('\n')[:-1]
 
 
 # =============================================================================
 # PART 1
 # =============================================================================
 
-nbLines = reports.shape[0]
+nbLines = len(reports)
 halfNbLines = nbLines // 2
-nbBits  = reports.shape[1]
+nbBits  = len(reports[0])
 
 gammaBinary = '0b'
 for i in range(nbBits):
-    nbOnes = np.sum(reports[:,i])
+    nbOnes = sum(list(map(
+        lambda x : int(x[i]),reports
+        )))
     gammaBinary += '1' if nbOnes > halfNbLines else '0'
 
 gamma = int(gammaBinary,2)
 epsilon = int('0b'+'1'*nbBits,2) - gamma
 
 powerConsumption = gamma*epsilon
-print(powerConsumption)
+print(powerConsumption) # 3,958,454
 
 # =============================================================================
 # PART 2
 # =============================================================================
 
-remainers = np.copy(reports)
+def getRemainers(previousRemainers,leastCommon,defaultValue):
+    
+    nbOnes = sum(list(map(
+        lambda x : int(x[i]),previousRemainers
+        )))
+    
+    halfLen = len(previousRemainers)/2
+    
+    if nbOnes > halfLen:
+        mostCommonValue = '0' if leastCommon else '1'
+    elif nbOnes < halfLen:
+        mostCommonValue = '1' if leastCommon else '0'
+    else:
+        mostCommonValue = defaultValue
+        
+    
+    remainers = list(filter(lambda x : x[i] == mostCommonValue,previousRemainers))
+    return remainers
+
+    
+remainersOxygen = reports[:]
+remainersCO2 = reports[:]
+valueOxygen = -1
+valueCO2 = -1
+
 for i in range(nbBits):
-    nbOnes = sum(remainers[:,i])
-    mostCommon = '1' if nbOnes > halfNbLines else '0'
-    remainers = np.array()
+    
+    if valueOxygen == -1:
+        remainersOxygen = getRemainers(remainersOxygen,False,'1')
+        print(remainersOxygen)
+    if valueCO2 == -1:
+        remainersCO2 = getRemainers(remainersCO2,True,'0')
+            
+    if len(remainersOxygen) == 1:
+        valueOxygen = remainersOxygen[0]
+        
+    if len(remainersCO2) == 1:
+        valueCO2 = remainersCO2[0]
+    
+    if valueOxygen != -1 and valueCO2 != -1:
+        break
+
+print(valueOxygen, valueCO2)
+print(int(valueOxygen,2) * int(valueCO2,2)) # 1,613,181
